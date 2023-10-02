93ff873c3212599fadfa92de0f4fbb05281e9447
PythonZem <pythonzem@gmail.com>
2023-10-02 17:22:10 +0300

    import moduls for using veribal env.

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+TEQUILA_KEY = os.getenv("FLIGHT_SEARCH_KEY")
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     pass
