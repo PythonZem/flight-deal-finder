4a8f615434a754a5ff84ffbb4099d31ef593bdec
pavluchkina <114096673+PythonZem@users.noreply.github.com>
2023-10-03 15:20:23 +0300

    finish edit FlightSearch class

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import find_dotenv, load_dotenv
+from flight_search import FlightSearch
+
+load_dotenv(find_dotenv())
+FLIGHT_SEARCH_KEY = os.getenv("FLIGHT_SEARCH_KEY")
+
+
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
