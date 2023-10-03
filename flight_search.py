2a11c61eb2d53c3e84a745fc253dce70960a6c2d
PythonZem <pythonzem@gmail.com>
2023-10-03 09:01:50 +0300

    Update flight_search.py

--- flight_search.py
+++ flight_search.py
@@ -1,9 +1,13 @@
 import os
+import requests
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
 TEQUILA_KEY = os.getenv("FLIGHT_SEARCH_KEY")
 class FlightSearch:
+
+    def __init__(self):
+        self.fly_search_endpoint = "https://api.tequila.kiwi.com/v2"
     #This class is responsible for talking to the Flight Search API.
     pass
