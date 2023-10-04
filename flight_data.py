1a8e52d146e69a2cebbc2cff3d64743424459384
PythonZem <114096673+PythonZem@users.noreply.github.com>
2023-10-04 09:56:21 +0300

    add true format for time dep. and arr

--- flight_data.py
+++ flight_data.py
@@ -3,8 +3,8 @@ class FlightData:
     def __init__(self, fly_from: str, fly_to: str, utc_departure: str, utc_arrival: str, price: str):
         self.fly_from = fly_from
         self.fly_to = fly_to
-        self.utc_departure = utc_departure
-        self.utc_arrival = utc_arrival
+        self.utc_departure = f"{utc_departure[:10]} {utc_departure[11:19]}"
+        self.utc_arrival = f"{utc_arrival[:10]} {utc_arrival[11:19]}"
         self.price = price
 
 
