53273eec7dbab6a732517f43a77988299620eb6a
PythonZem <114096673+PythonZem@users.noreply.github.com>
2023-10-04 10:16:23 +0300

    add notification for class and main

--- main.py
+++ main.py
@@ -2,13 +2,37 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from flight_search import FlightSearch
 from flight_data import FlightData
+from data_manager import DataManager
+from notification_manager import NotificationManager
 
 load_dotenv(find_dotenv())
 
-data = FlightSearch(API_KEY=os.getenv("FLIGHT_SEARCH_KEY"))
-d = data.fly_searching(fly_from="IST", fly_to="PAR", date_from="04/10/2023", date_to="04/03/2024")
+FLY_KEY = os.getenv("FLIGHT_SEARCH_KEY")
+TWILIO_KYE = os.getenv("TWILIO_KYE")
+TWILIO_TOKEN = os.getenv("TWILIO_TOKEN")
 
-f = FlightData(fly_from=d["flyFrom"], fly_to=d["flyTo"], utc_departure=d["utc_departure"],
-               utc_arrival=d["utc_arrival"], price=d["price"])
-print(f.price)
+FlyData_SearchModule = FlightSearch(API_KEY=FLY_KEY)
+sheety_data = DataManager()
+target_cities_list = sheety_data.get_request()
+
+
+def start_program():
+
+    for target_city in target_cities_list:
+        fly_fromCity = "Warsaw"
+        fly_fromCode = "WAW"
+        fly_toCode = target_city["iataCode"]
+        fly_toCity = target_city["city"]
+        target_price = target_city["lowestPrice"]
+
+        s_data = FlyData_SearchModule.fly_searching(fly_from=fly_fromCode, fly_to=fly_toCode,
+                                                    date_from="04/10/2023", date_to="04/03/2024")
+
+        flyData = FlightData(fly_from=s_data["flyFrom"], fly_to=s_data["flyTo"], utc_departure=s_data["utc_departure"],
+                             utc_arrival=s_data["utc_arrival"], price=s_data["price"])
+        if float(flyData.price) <= target_price:
+            text_sms = f"""Low price alert! Only €{flyData.price} to fly from {fly_fromCity}-{flyData.fly_from} to 
+    {fly_toCity}-{fly_toCode}, from {flyData.utc_arrival} to {flyData.utc_departure}"""
+            print(text_sms)
+        # Sender_SMS = NotificationManager(KEY=TWILIO_KYE, TOKEN=TWILIO_TOKEN, text_sms=text_sms)
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,19 @@
+from twilio.rest import Client
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
+    def __init__(self, KEY: str, TOKEN: str, text_sms: str):
+        self.account_sid = KEY
+        self.auth_token = TOKEN
+        self.text_sms = text_sms
+        self.send_sms()
+
+    def send_sms(self):
+        client = Client(self.account_sid, self.auth_token)
+
+        message = client.messages.create(
+            to='+380503962864',
+            body=self.text_sms
+        )
+
     pass
