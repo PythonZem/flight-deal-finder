b2d6f4acb7f7c27a152301821f2b819239f8b909
PythonZem <pythonzem@gmail.com>
2023-10-09 22:19:49 +0300

    add class user

---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+
+    def __init__(self, first_name: str, last_name: str, email: str):
+        self.fist_name = first_name
+        self.last_name = last_name
+        self.email = email
+
